1084b8276e9cfc573f4999ab90ad572f934f601f
SungWook Yang <murcielago1727@gmail.com>
2021-04-09 05:48:05 +0000

    make password encrypt logic use bcrypt

--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,10 +31,34 @@ const userSchema = mongoose.Schema({
   // token 유효기간
   tokenExp: {
     type: Number
+  },
+});
+
+// save()실행전에 먼저 실행된다.
+// callback으로 반드시 function키워드를 사용한 함수를 전달해야한다.
+userSchema.pre('save', function(next) {
+  var user = this;
+  
+  // pw를 변경할 경우에만 암호화 로직 실행
+  if(user.isModified("password")) {
+  
+    // 비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err)
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if(err) return next(err)
+
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
   }
 });
 
 // 작성한 mongoDB 스키마 생성
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
